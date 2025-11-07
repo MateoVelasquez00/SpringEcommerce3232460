6a4207765e14b6b5d1dc0f2f09253af2290a8990
MateoVelasquez00 <mv202642@gmail.com>
2025-11-07 12:58:56 -0500

    v0.2_configuracionAplication.properties&conexionaBaseDeDatos_MateoVelasquez_07112025

--- src/main/java/com/sena/springecommerce/model/DetalleOrden.java
+++ src/main/java/com/sena/springecommerce/model/DetalleOrden.java
@@ -1,32 +1,104 @@
 package com.sena.springecommerce.model;
 
-import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "usuarios")
+@Table(name = "detalles")
 public class DetalleOrden {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // Anotacion JPA
 	private Integer id;
 	private String nombre;
-	private String telefono;
-	private String email;
-	private String direccion;
-	private String rol;
-	private String password;
+	private Double cantidad;
+	private Double precio;
+	private Double total;
+	
+	@ManyToOne
+	private Orden orden;
+	
+	@ManyToOne 
+	private Producto producto;
 
-	// Relaciones en DB
-	@OneToMany(mappedBy = "usuario")
-	private List<Producto> productos;
+	public DetalleOrden() {
 
-	@OneToMany(mappedBy = "usuario")
-	private List<Orden> ordenes;
+	}
 
+	public DetalleOrden(Integer id, String nombre, Double cantidad, Double precio, Double total) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+		this.precio = precio;
+		this.total = total;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Double getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Double cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
+	public Orden getOrden() {
+		return orden;
+	}
+
+	public void setOrden(Orden orden) {
+		this.orden = orden;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "DetalleOrden [id=" + id + ", nombre=" + nombre + ", cantidad=" + cantidad + ", precio=" + precio
+				+ ", total=" + total + "]";
+	}
+	
+	
 }
